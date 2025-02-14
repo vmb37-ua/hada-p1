1643d00c8cbfa28724d1936b9b266bf28fcbc898
Victor Mingorance Boix <vmb37@alu.ua.es>
2025-02-14 19:10:24 +0100

    Modified Seconds2Minutes if statement

--- hada-p1/HadaP1.cs
+++ hada-p1/HadaP1.cs
@@ -9,6 +9,7 @@ namespace hada_p1
     internal class HadaP1
     {
         public static double Seconds2Minutes(double s) {
+            if (s == 0) return 0;
             return s / 60;
         }
     }
