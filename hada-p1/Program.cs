5c2353fdd3fc008c126eb7dbe37ea48a8f0203c2
Victor Mingorance Boix <vmb37@alu.ua.es>
2025-02-14 20:30:22 +0100

    Added main body to Program

--- hada-p1/Program.cs
+++ hada-p1/Program.cs
@@ -10,6 +10,36 @@ namespace hada_p1
     {
         static void Main(string[] args)
         {
+            string respuesta;
+            double cant;
+
+            do
+            {
+                Console.Write("Introduce unidad a convertir (m / s): ");
+                respuesta = Console.ReadLine();
+                Console.Write("Introduce cantidad: ");
+                cant = double.Parse(Console.ReadLine());
+
+                double resultado;
+
+                if (respuesta == "m")
+                {
+                    resultado = HadaP1.Minutes2Seconds(cant);
+                    Console.WriteLine("Son {0} segundos.", resultado);
+                }
+                else if (respuesta == "s")
+                {
+                    resultado = HadaP1.Seconds2Minutes(cant);
+                    Console.WriteLine("Son {0} minutos.", resultado);
+                }
+                else
+                {
+                    Console.WriteLine("Error, unidad no válida.");
+                }
+
+                Console.WriteLine("Deseas volver a calcular un valor? (S/N): ");
+                respuesta = Console.ReadLine();
+            } while (respuesta != "N");
         }
     }
 }
