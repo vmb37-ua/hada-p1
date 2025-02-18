bde4f98b38640836c5080e5a472c92e39407edfe
Victor Mingorance Boix <vmb37@alu.ua.es>
2025-02-18 11:13:18 +0100

    Added docs to HadaP1.cs

--- hada-p1/HadaP1.cs
+++ hada-p1/HadaP1.cs
@@ -6,13 +6,26 @@ using System.Threading.Tasks;
 
 namespace hada_p1
 {
+    /// <summary>
+    /// Clase que sirve como apoyo para cálculo de conversión entre unidades de tiempo.
+    /// </summary>
     internal class HadaP1
     {
+        /// <summary>
+        /// Función de conversión de segundos a minutos.
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
         public static double Seconds2Minutes(double s) {
             if (s == 0) return 0;
             return s / 60;
         }
 
+        /// <summary>
+        /// Función de conversión de minutos a segundos.
+        /// </summary>
+        /// <param name="m"></param>
+        /// <returns></returns>
         public static double Minutes2Seconds(double m) {
             return 60 * m;
         }
